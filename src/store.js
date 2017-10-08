d9e40e7353dd4a47f0dc0c64992ef31d6a437d8f
Shawn Krisman <shawn@patreon.com>
2017-10-07 23:39:06 -0700

    added redux saga

--- src/store.js
+++ src/store.js
@@ -1,10 +1,32 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { call, put } from 'redux-saga/effects'
+
+const animationFrame = () => {
+    return new Promise(resolve => {
+        window.requestAnimationFrame(resolve)
+    })
+}
+
+function* tick() {
+    let time = Date.now()
+    while (true) {
+        yield call(animationFrame)
+        let newTime = Date.now()
+        yield put({ type: 'TICK', dt: newTime - time })
+        time = newTime
+    }
+}
+
+const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
     store => store,
     { gold: 5 },
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeEnhancers(applyMiddleware(sagaMiddleware)),
 )
+
+sagaMiddleware.run(tick)
 
 export default store
