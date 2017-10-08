74c88bd3856f882d85af761fd10389f665644a7f
Shawn Krisman <shawn@patreon.com>
2017-10-08 01:19:33 -0700

    wrote gather wood component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import './App.css'
 import store from './store'
-import Gold from './components/gold'
+import GatherWood from './components/gatherWood'
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
                 <div className="App">
-                    <Gold />
+                    <GatherWood />
                 </div>
             </Provider>
         )
---
+++ src/components/gatherWood.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class GatherWood extends Component {
+    render() {
+        const wood = 0
+        return (
+            <p>
+                Gather Wood: {wood} + -
+            </p>
+        )
+    }
+}
+
+export default GatherWood
