a5d130e6144cc2b41e755933698dd318df825b07
Shawn Krisman <shawn@patreon.com>
2017-10-08 02:10:25 -0700

    finished counter.

--- src/components/gatherWood.js
+++ src/components/gatherWood.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class GatherWood extends Component {
     render() {
-        const wood = 0
+        const {
+            woodWorkers,
+            canAddWorkers,
+            canRemoveWorkers,
+            addWorker,
+            removeWorker,
+        } = this.props
         return (
             <p>
-                Gather Wood: {wood}
-                <button>+</button>
-                <button>-</button>
+                Gather Wood: {woodWorkers}
+                <button disabled={!canAddWorkers} onClick={addWorker}>
+                    +
+                </button>
+                <button disabled={!canRemoveWorkers} onClick={removeWorker}>
+                    -
+                </button>
             </p>
         )
     }
 }
 
-export default GatherWood
+const mapStateToProps = state => {
+    const { workers } = state
+    return {
+        woodWorkers: workers.wood,
+        canAddWorkers: workers.idle >= 1,
+        canRemoveWorkers: workers.wood > 0,
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    addWorker: () => {
+        dispatch({ type: 'ADD_WORKER', resource: 'wood' })
+    },
+    removeWorker: () => {
+        dispatch({ type: 'REMOVE_WORKER', resource: 'wood' })
+    },
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(GatherWood)
--- src/reducers/workers.js
+++ src/reducers/workers.js
@@ -7,6 +7,18 @@ export default (state = { idle: 10, wood: 0 }, action) => {
                 ...state,
                 idle: state.idle + action.dt / 1000 * WORKERS_PER_SECOND,
             }
+        case 'ADD_WORKER':
+            return {
+                ...state,
+                idle: state.idle - 1,
+                [action.resource]: state[action.resource] + 1,
+            }
+        case 'REMOVE_WORKER':
+            return {
+                ...state,
+                idle: state.idle + 1,
+                [action.resource]: state[action.resource] - 1,
+            }
         default:
             return state
     }
